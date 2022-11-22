9bbb53ef926068a584729db50166cc8741685ff2
Maks Tkachyk <tkachykmakc@gmail.com>
2022-11-22 18:21:41 +0200

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      Second steptrhrhrthrth
+      Second steptrhrhrthrthrtrtrhrtrrthrthrthtrhrttrh
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
