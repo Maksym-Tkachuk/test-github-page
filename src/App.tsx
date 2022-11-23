a68e0db1898f6e3a4899dd2e9e0dd33550eb7f94
Maks Tkachyk <tkachykmakc@gmail.com>
2022-11-23 15:26:44 +0200

    Some commit

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">hhhh2222222222hhhhhI</p>
+      <p className="read-the-docs">hhhh22222hhhhhI</p>
     </div>
   );
 }
