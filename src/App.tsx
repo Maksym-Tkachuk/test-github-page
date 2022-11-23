7217f311851716c689b22410306daf02bb3e02c4
Maks Tkachyk <tkachykmakc@gmail.com>
2022-11-23 09:42:03 +0200

    New enviroments

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      New Branch
+      Delete hompage
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
